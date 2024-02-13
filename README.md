b4457fb42af949a8c3c6ad526dc5e5bf23a28597
Dan Williams <dan@dotfive.co.uk>
2024-02-13 08:18:52 +0000

    Initial commit
      - Populated required fields in Cargo.toml.
    
      - Added build options to optimise compilation speed and compiled
        executable size.
    
      - Added README stub and CHANGELOG stub.
    
      - Added MIT license.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Patchify
+
+The Patchify crate is an auto-update library, providing the ability for Rust
+applications to automatically update themselves.
+
+
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! The Patchify crate is an auto-update library, providing the ability for Rust
+//! applications to automatically update themselves.
+//! 
+
+
