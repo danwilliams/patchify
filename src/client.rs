061acba307409d3afc27db0b0260cd008c5cba86
Dan Williams <dan@dotfive.co.uk>
2024-02-22 06:37:21 +0000

    Implemented Drop for Updater, using message queue
      - Added an MPSC queue to the Updater, using flume. This is to provide
        a means of communicating with the background interval timer thread.
    
      - Implemented Drop for Updater, to simply send a message to the queue.
        Sending the message implies a shutdown is required.
    
      - Added handling of message queue events to the event-handling loop
        set up in Updater::new(). Receiving any message will exit the loop,
        effectively shutting the timer thread down.
    
      - Added documentation, but no tests for this functionality at present.

--- src/client.rs
+++ src/client.rs
@@ -11,6 +11,7 @@ mod tests;
 //		Packages
 
 use ed25519_dalek::VerifyingKey;
+use flume::{Sender, self};
 use reqwest::Url;
 use semver::Version;
 use tokio::{
@@ -65,6 +66,11 @@ pub struct Updater {
 	//		Private properties													
 	/// The configuration for the updater service.
 	config: Config,
+	
+	/// The updater queue that is used for communicating with the interval
+	/// timer. This is the sender side only. A queue is used so that the timer
+	/// can run in a separate thread, but be stopped when required.
+	queue:  Sender<()>,
 }
 
 //󰭅		Updater																	
@@ -76,27 +82,46 @@ impl Updater {
 	/// configuration. As soon as the service is created, it will start checking
 	/// for updates.
 	/// 
+	/// In order to shut down nicely, [`Drop`] is implemented, and will send a
+	/// message to the internal queue to stop the timer.
+	/// 
 	/// # Parameters
 	/// 
 	/// * `config` - The configuration for the updater service.
 	/// 
 	#[must_use]
 	pub fn new(config: Config) -> Self {
-		let updater = Self { config };
+		let (sender, receiver) = flume::unbounded();
+		let updater            = Self { config, queue: sender };
 		if updater.config.check_on_startup {
 			info!("Checking for updates");
 		}
 		if let Some(check_interval) = updater.config.check_interval {
 			let mut timer = interval(check_interval);
-			//	Interval-based loop
+			//	Event-handling loop
 			drop(spawn(async move { loop { select!{
+				//	Wait for timer to tick
 				_ = timer.tick() => {
 					info!("Checking for updates");
+				}
+				//	Wait for message from queue - this is a blocking call
+				_ = receiver.recv_async() => {
+					info!("Stopping updater");
+					break;
 				}
 			}}}));
 		}
 		updater
 	}
+}
+
+//󰭅		Drop																	
+impl Drop for Updater {
+	//		drop																
+    fn drop(&mut self) {
+		//	Send a message to the queue to stop the timer, ignoring any errors
+		let _ignored = self.queue.send(());
+    }
 }
 
 
