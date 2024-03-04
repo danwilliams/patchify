a447898c7ecec00d4a67b67c41300d5ad0d8e2b0
Dan Williams <dan@dotfive.co.uk>
2024-03-04 13:34:20 +0000

    Added extended integration test scenarios

--- tests/client.rs
+++ tests/client.rs
@@ -9,13 +9,17 @@ mod common;
 
 //		Packages
 
-use crate::common::client::request;
-use ed25519_dalek::VerifyingKey;
+use crate::common::{client::request, utils::*};
+use ed25519_dalek::{Signer, VerifyingKey};
 use once_cell::sync::Lazy;
-use patchify::client::{Config, Updater};
+use patchify::client::{Config, Status, Updater};
 use reqwest::StatusCode;
 use semver::Version;
+use serde_json::json;
+use sha2::{Sha256, Digest};
 use std::{
+	env::current_exe,
+	fs,
 	io::{BufReader, BufRead},
 	net::SocketAddr,
 	process::{Command, Stdio},
@@ -165,6 +169,111 @@ mod actions {
 			check_interval:   Some(Duration::from_millis(50)),
 		}).unwrap();
 		sleep(Duration::from_millis(125)).await;
+	}
+	
+	//		check_for_updates													
+	#[tokio::test]
+	async fn check_for_updates__complete_successful_process() {
+		//	No test for this at present, as we can't restart from within the test
+		//	environment. This is tested using the testbins full integration tests,
+		//	and for the "normal" integration tests the behaviour up to the point of
+		//	restart is tested.
+	}
+	#[tokio::test]
+	async fn check_for_updates__no_update_available() {
+		let mock_server = MockServer::start().await;
+		let private_key = generate_new_private_key();
+		let json_data   = json!({
+			"version": "1.0.0",
+		});
+		Mock::given(method("GET"))
+			.and(path("/api/latest"))
+			.respond_with(
+				ResponseTemplate::new(200)
+					.append_header("Content-Type", "application/json")
+					.append_header("X-Signature",  private_key.sign(json_data.to_string().as_ref()).to_string())
+					.set_body_json(json_data)
+			)
+			.expect(1)
+			.mount(&mock_server).await
+		;
+		let updater = Updater::new(Config {
+			version:          Version::new(1, 0, 0),
+			api:              format!("{}/api/", mock_server.uri()).parse().unwrap(),
+			key:              private_key.verifying_key(),
+			check_on_startup: true,
+			check_interval:   None,
+		}).unwrap();
+		sleep(Duration::from_millis(50)).await;
+		//	TODO: Should gain more insight into the outcome at some point, through
+		//	TODO: error status or similar
+		assert_eq!(updater.status(), Status::Idle);
+	}
+	#[tokio::test]
+	async fn check_for_updates__restart_blocked() {
+		let mock_server = MockServer::start().await;
+		let version     = Version::new(2, 3, 4);
+		let private_key = generate_new_private_key();
+		let payload     = b"Test payload";
+		let json_data1  = json!({
+			"version": version,
+		});
+		let json_data2  = json!({
+			"version": version,
+			"hash":    hex::encode(Sha256::digest(payload)),
+		});
+		Mock::given(method("GET"))
+			.and(path("/api/latest"))
+			.respond_with(
+				ResponseTemplate::new(200)
+					.append_header("Content-Type", "application/json")
+					.append_header("X-Signature",  private_key.sign(json_data1.to_string().as_ref()).to_string())
+					.set_body_json(json_data1)
+					//	Delay slightly to allow registration of the critical action
+					.set_delay(Duration::from_millis(1))
+			)
+			.expect(1)
+			.mount(&mock_server).await
+		;
+		Mock::given(method("GET"))
+			.and(path(format!("/api/releases/{version}")))
+			.respond_with(
+				ResponseTemplate::new(200)
+					.append_header("Content-Type", "application/octet-stream")
+					.set_body_bytes(payload.to_vec())
+			)
+			.expect(1)
+			.mount(&mock_server).await
+		;
+		Mock::given(method("GET"))
+			.and(path(format!("/api/hashes/{version}")))
+			.respond_with(
+				ResponseTemplate::new(200)
+					.append_header("Content-Type", "application/json")
+					.append_header("X-Signature",  private_key.sign(json_data2.to_string().as_ref()).to_string())
+					.set_body_json(json_data2)
+			)
+			.expect(1)
+			.mount(&mock_server).await
+		;
+		let updater = Updater::new(Config {
+			version:          Version::new(1, 0, 0),
+			api:              format!("{}/api/", mock_server.uri()).parse().unwrap(),
+			key:              private_key.verifying_key(),
+			check_on_startup: true,
+			check_interval:   None,
+		}).unwrap();
+		updater.register_action();
+		sleep(Duration::from_millis(100)).await;
+		//	We've registered a critical action, so the installation will be blocked,
+		//	which is what we want here, so that we can check the status is correct
+		assert_eq!(updater.status(), Status::PendingRestart(version.clone()));
+		//	Assuming the update process ran correctly, we now need to rename the
+		//	test binary back to its original name, so that the next test can run
+		let path = current_exe().unwrap();
+		if path.file_name().unwrap().to_str().unwrap().ends_with(".old") {
+			fs::rename(path.clone(), path.with_extension("")).unwrap();
+		}
 	}
 }
 
